9e6da95bdf33b7c00f609cb0b657c49406187b43
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-02-17 16:49:50 +0530

    added shoxcity()

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 // import "./styles.css";
 
@@ -39,7 +38,7 @@ function App() {
     { name: "Tirupati", country: "India" }
   ];
 
-  var newList1 = cityList.filter(function (ele) {
+  /* var newList1 = cityList.filter(function (ele) {
     return ele.country === "India";
   });
 
@@ -49,7 +48,7 @@ function App() {
 
   var combList = [...newList1, ...newList2];
 
-  let unique = [...new Set(combList.map((item) => item.name))];
+  let unique = [...new Set(combList.map((item) => item.name))]; */
   /*var unique = []; 
   newList.map((elem) => {
     if (unique.indexOf(elem.name) === -1) {
@@ -57,19 +56,62 @@ function App() {
     }
   }); */
 
-//   let i = 1;
-//   var result = unique.map(function (el) {
-//     var o = Object.assign({}, el);
-//     o.key = "location" + i;
-//     i++;
-//     return o;
-//   });
+  //   let i = 1;
+  //   var result = unique.map(function (el) {
+  //     var o = Object.assign({}, el);
+  //     o.key = "location" + i;
+  //     i++;
+  //     return o;
+  //   });
 
-//   console.log(cityList);
-//   console.log(newList);
-//   console.log(unique);
-//   console.log(result);
-  
+  //   console.log(cityList);
+  //   console.log(newList);
+  //   console.log(unique);
+  //   console.log(result);
+
+  //   function showCity() {
+  //     let new_city = cityList.filter((location) => {
+  //       if (location.country === "India") return location;
+  //     });
+  //     let result = new_city.map((city, index) => (
+  //       <li key={`location${index + 1}`}>{city.name}</li>
+  //     ));
+  //     return result;
+  //   }
+
+  //   return (
+  //     <div id="main">
+  //       <ol>
+  //         {showCity()}
+  //         {/* // //         {unique.map((item, index) => ( */}
+  //         {/* //           <li key={`location${index + 1}`}>
+  // //             {item[0]}
+  // //             {item[1]}
+  // //             {item[2]}
+  // //             {item[3]}
+  // //             {item[4]}
+  // //             {item[5]}
+  // //             {item[6]}
+  // //             {item[7]}
+  // //             {item[8]}
+  // //             {item[9]}
+  // //             {item[10]}
+  // //             {item[11]}
+  // //             {item[12]}
+  // //             {item[13]}
+  // //             {item[14]}
+  // //             {item[15]}
+  // //             {item[16]}
+  // //             {item[17]}
+  // //             {item[18]}
+  // //             {item[19]}
+  // //           </li>
+  //           // <li key={item.key}>{item.map((i)=>({i}))}</li>
+  // //         ))} */}
+  //       </ol>
+  //     </div>
+  //   );
+
   function showCity() {
     let new_city = cityList.filter((location) => {
       if (location.country === "India") return location;
@@ -80,39 +122,13 @@ function App() {
     return result;
   }
 
+  // render() {
   return (
     <div id="main">
-      <ol>
-        {showCity()}
-//         {unique.map((item, index) => (
-//           <li key={`location${index + 1}`}>
-//             {item[0]}
-//             {item[1]}
-//             {item[2]}
-//             {item[3]}
-//             {item[4]}
-//             {item[5]}
-//             {item[6]}
-//             {item[7]}
-//             {item[8]}
-//             {item[9]}
-//             {item[10]}
-//             {item[11]}
-//             {item[12]}
-//             {item[13]}
-//             {item[14]}
-//             {item[15]}
-//             {item[16]}
-//             {item[17]}
-//             {item[18]}
-//             {item[19]}
-//           </li>
-          // <li key={item.key}>{item.map((i)=>({i}))}</li>
-//         ))}
-      </ol>
+      <ol>{showCity()}</ol>
     </div>
   );
+  // }
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-// function generateCodeFromObject(obj){
-//     //return a code generated string
-//    }
-   
-// module.exports=generateCodeFromObject;
+function generateCodeFromObject(obj) {
+  return obj;
+  //return a code generated string
+}
 
+module.export = generateCodeFromObject;
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById("root"));
